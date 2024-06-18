37677af6227f638703ccd98ff2d9cc7a1200524a
ryncka <ryncka@outlook.com>
2024-06-18 16:05:48 +0800

    docs

--- constraints.go
+++ constraints.go
@@ -1,17 +1,21 @@
 package flex
 
+// Numeric represents a numeric type, which can be signed, unsigned, or a float.
 type Numeric interface {
 	Signed | Unsigned | Float
 }
 
+// Signed represents a signed integer type.
 type Signed interface {
 	~int8 | ~int16 | ~int32 | ~int64 | ~int
 }
 
+// Unsigned represents an unsigned integer type.
 type Unsigned interface {
 	~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uint
 }
 
+// Float represents a floating-point type.
 type Float interface {
 	~float32 | ~float64
 }
--- stack.go
+++ stack.go
@@ -6,14 +6,16 @@ type Stack[T any] struct {
 	elements []T
 }
 
+// NewStack initializes a new stack with the specified maximum length.
 func NewStack[T any](length uint32) *Stack[T] {
 	return &Stack[T]{
 		top:      -1,
 		length:   length,
-		elements: make([]T, length),
+		elements: make([]T, length), // Initialize the elements slice with zero values
 	}
 }
 
+// Push adds a new item onto the stack.
 func (stack *Stack[T]) Push(item T) bool {
 	if stack.Full() {
 		return false
@@ -23,6 +25,7 @@ func (stack *Stack[T]) Push(item T) bool {
 	return true
 }
 
+// DynamicPush adds a new item onto the stack and dynamically expands the stack if it's full.
 func (stack *Stack[T]) DynamicPush(item T) bool {
 	if stack.Full() {
 		var (
@@ -30,7 +33,7 @@ func (stack *Stack[T]) DynamicPush(item T) bool {
 			newLength   = oldLength * 2
 			newElements = make([]T, newLength)
 		)
-		// Copy old-element to new-element
+		// Copy existing elements to the new slice
 		for i := uint32(0); i < oldLength; i++ {
 			newElements[i] = stack.elements[i]
 		}
@@ -42,6 +45,7 @@ func (stack *Stack[T]) DynamicPush(item T) bool {
 	return true
 }
 
+// Peek returns the top item of the stack without removing it.
 func (stack *Stack[T]) Peek() (item T, ok bool) {
 	if stack.Empty() {
 		return
@@ -49,6 +53,7 @@ func (stack *Stack[T]) Peek() (item T, ok bool) {
 	return stack.elements[stack.top], true
 }
 
+// Pop removes and returns the top item from the stack.
 func (stack *Stack[T]) Pop() (item T, ok bool) {
 	if stack.Empty() {
 		return
@@ -58,14 +63,17 @@ func (stack *Stack[T]) Pop() (item T, ok bool) {
 	return item, true
 }
 
+// Empty checks if the stack is empty.
 func (stack *Stack[T]) Empty() bool {
 	return stack.top == -1
 }
 
+// Full checks if the stack is full.
 func (stack *Stack[T]) Full() bool {
 	return stack.top+1 == int64(stack.length)
 }
 
+// Length returns the current number of items in the stack.
 func (stack *Stack[T]) Length() int64 {
 	return stack.top + 1
 }
